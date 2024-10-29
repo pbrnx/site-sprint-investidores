4d13a80f37a1aa5e12fc6856e07e64a0b3a208fe
Pedro Barone <pedrobarone2709@gmail.com>
2024-10-29 10:46:59 -0300

    env2

--- src/routes/Cadastro.jsx
+++ src/routes/Cadastro.jsx
@@ -91,7 +91,7 @@ function Cadastro() {
     };
 
     try {
-      const response = await fetch(`${process.env.NGROK_URL}/users`, {
+      const response = await fetch(`${process.env.REACT_APP_NGROK_URL}/users`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',

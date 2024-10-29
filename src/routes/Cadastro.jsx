71035eb4108fcba315ca36ab1870285715e6c69f
Pedro Barone <pedrobarone2709@gmail.com>
2024-10-29 09:08:49 -0300

    esconder API endpoint do frontend

--- src/routes/Cadastro.jsx
+++ src/routes/Cadastro.jsx
@@ -91,7 +91,7 @@ function Cadastro() {
     };
 
     try {
-      const response = await fetch('https://fe3f-2804-7f0-36-11e3-5c6a-1684-3925-140e.ngrok-free.app/users', {
+      const response = await fetch('NGROK_URL', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
